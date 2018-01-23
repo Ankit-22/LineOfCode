f9eedfaaf5702f15cec4805d49aad9b8103246d6
Ankit Tripathi <Ankitmani.t@somaiya.edu>
2018-01-23 15:20:57 +0530

    Add Traverse Directory

---
+++ traverseDirectory.js
@@ -0,0 +1,28 @@
+var gitDirectoryScrapper = directoryUrl => {
+	directoryPromise = new Promise( (resolve, reject) => {
+		$.ajax({
+			url: directoryUrl,
+			success: result => {
+				resolve(result);
+			},
+			error: (jqXHR, textStatus, errorThrown) => {
+				reject(textStatus);
+			},
+		});
+	});
+
+	directoryPromise.then(
+		data => {
+			data.forEach( fileObject => {
+				if(fileObject.type == "dir")
+					gitDirectoryScrapper(fileObject.url);
+				else console.log(fileObject.path);
+			});
+		},
+		error => {
+			console.log(error);
+		}
+	);
+};
+
+gitDirectoryScrapper("https://api.github.com/repos/Ankit-22/AutoConnect/contents/");
