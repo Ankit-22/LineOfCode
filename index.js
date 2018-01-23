dbf6df7c224aa50435b056b164ec0596348e5b33
Ankit Tripathi <Ankitmani.t@somaiya.edu>
2018-01-23 12:32:07 +0530

    Pretiffy Promise Syntax

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-var getLinesOfCodePromise = (gitUrl)=>{
-		return new Promise(function (resolve, reject) {
-		$.ajax(
-			{
-				url: gitUrl,
-				success: (result)=>{
-					var decoded_string = atob(result.content);
-					resolve(decoded_string.split("\n").length - 1);
-				},
-				error: (jqXHR, textStatus, errorThrown)=>{
-					reject(textStatus);
-				}
-			}
-		);
+var getLinesOfCodePromise = gitUrl => {
+	return new Promise(function(resolve, reject) {
+		$.ajax({
+			url: gitUrl,
+			success: result => {
+				var decoded_string = atob(result.content);
+				resolve(decoded_string.split('\n').length - 1);
+			},
+			error: (jqXHR, textStatus, errorThrown) => {
+				reject(textStatus);
+			},
+		});
 	});
 };
 
-var getLinesOfCode = getLinesOfCodePromise("https://api.github.com/repos/Ankit-22/AutoConnect/git/blobs/e6b32bc7884bb98a5024981d37fec787514b56c8");
-getLinesOfCode.then((data)=>{
+var getLinesOfCode = getLinesOfCodePromise(
+	'https://api.github.com/repos/Ankit-22/AutoConnect/git/blobs/e6b32bc7884bb98a5024981d37fec787514b56c8'
+);
+
+getLinesOfCode.then(
+	data => {
 		console.log(data);
-	}, (error)=>{
+	},
+	error => {
 		console.log(error);
-});
+	}
+);
